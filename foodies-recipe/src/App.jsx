107f73d2c9814c4e36c8b9764a535f676f1baf2d
Adhitya Hasan <adhityahasan16@mhs.mdp.ac.id>
2023-10-23 21:13:42 +0700

    add navbar, package, and home

--- foodies-recipe/src/App.jsx
+++ foodies-recipe/src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Home from "./components/home/home";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
+      <Home />
       <h1 className="text-center text-2xl font-bold ">Hai</h1>
     </>
   );
---
+++ foodies-recipe/src/components/navbar/navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "../../assets/icon/foodies-icon.svg";
+import { Tooltip } from "antd";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav class=" flex justify-center h-[100px] ">
+        <div class="w-[1240px] flex items-center justify-between">
+          <div class="flex items-center w-[840px] justify-between ">
+            <img src={logo} alt="" width="186" height="42"></img>
+            <div class="flex w-[500px] justify-between">
+              <p class="text-purple-700">Home</p>
+              <p>Recipes</p>
+              <p>About</p>
+              <p>Contact</p>
+            </div>
+          </div>
+          <div class="w-[255px] flex justify-between">
+            <button class="px-[36px] py-[10px] rounded-[47px]">
+              <p class="text-purple-700">Sign in</p>
+            </button>
+            <button class="bg-purple-600 px-[36px] py-[10px] rounded-[47px]">
+              <p class="text-white">Register</p>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
